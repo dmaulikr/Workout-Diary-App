34c930c1e3bbdabcc00837ebab7de9f4eae3e644
lindgrenalex <alex.lindgren93@gmail.com>
2016-08-31 09:50:54 +0200

    Initial set up

---
+++ Source/WorkoutDiaryApp/WorkoutDiaryApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WorkoutDiaryApp
+//
+//  Created by Alexander Lindgren on 31/08/16.
+//  Copyright © 2016 Alexander Lindgren. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
